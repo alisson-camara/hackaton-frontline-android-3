4e8ce03c0015e12863f1ec7e7a87cf4b696440e7
Lucas Christopher <lucas.christopheer@gmail.com>
2024-12-18 16:06:58 -0300

    feat: removing function

--- src/main/kotlin/com/workshop/repository/RoomRepository.kt
+++ src/main/kotlin/com/workshop/repository/RoomRepository.kt
@@ -1,10 +1,14 @@
 package com.workshop.repository
 
+import com.workshop.db.PlayerTable
 import com.workshop.db.RoomTable
 import com.workshop.db.dao.RoomDAO
 import com.workshop.model.dto.RoomDTO
 import com.workshop.model.mapper.RoomMapper
 import com.workshop.utils.suspendTransaction
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.deleteWhere
 
 class RoomRepository : IRoomRepository {
 
@@ -33,8 +37,21 @@ class RoomRepository : IRoomRepository {
         return@suspendTransaction RoomMapper.mapDaoToDto(roomDao, playersDao)
     }
 
-    override suspend fun removePlayer(room: String, player: String): RoomDTO? {
-        TODO("Not yet implemented")
+    override suspend fun removePlayer(room: String, player: String) = suspendTransaction {
+        val roomWIthPlayers = RoomDAO
+            .find { (RoomTable.name eq player) }
+            .limit(1)
+            .firstOrNull()?.let { room: RoomDAO ->
+                val playersDao = room.players.toList()
+                room to playersDao
+            }
+
+        return@suspendTransaction roomWIthPlayers?.let { room ->
+            PlayerTable.deleteWhere {
+                (this.name eq player) and (RoomTable.id eq this.room)
+            }
+            RoomMapper.mapDaoToDto(room = room.first, players = room.second)
+        }
     }
 
     override suspend fun resetVotes(room: String, player: String): RoomDTO? {
@@ -51,7 +68,7 @@ class RoomRepository : IRoomRepository {
 
         players.forEach { it.point = "?" }
 
-        return RoomMapper.mapDaoToDto(roomDao,players)
+        return RoomMapper.mapDaoToDto(roomDao, players)
     }
 
     override suspend fun sendVote(room: String, player: String): RoomDTO? {
